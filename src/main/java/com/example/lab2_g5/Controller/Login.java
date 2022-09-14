e29fd9719b75a4b89fdcb100f89953cdd0293db3
MarceloJon <mrojasl@pucp.edu.pe>
2022-09-13 21:46:46 -0500

    pipipi

--- src/main/java/com/example/lab2_g5/Controller/Login.java
+++ src/main/java/com/example/lab2_g5/Controller/Login.java
@@ -1,2 +1,23 @@
-package com.example.lab2_g5.Controller;public class Login {
+package com.example.lab2_g5.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class Login {
+    @Autowired
+    UserRepository userRepository;
+
+    @PostMapping("verificar")
+    String verificar(@RequestParam("correo") String correo, @RequestParam("pswd") String pswd){
+        Optional<User> optionalUser = userRepository.encontrarUsuario(correo);
+        if (optionalUser.isPresent()){
+            User user = optionalUser.get();
+        }
+
+    }
+
 }
