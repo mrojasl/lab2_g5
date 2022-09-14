166771027389e5df24cfe22ec6f07a6b5bc255b5
david34Y <dgonzalesn@pucp.edu.pe>
2022-09-14 01:22:52 -0500

    Buscar transacciones

--- src/main/java/com/example/lab2_g5/Repository/TransactionRepository.java
+++ src/main/java/com/example/lab2_g5/Repository/TransactionRepository.java
@@ -3,12 +3,12 @@ package com.example.lab2_g5.Repository;
 import com.example.lab2_g5.Entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
-    @Query(value = "select idtransaction, neetwork_fee, amount, block, currency, status, wallet_from from transaction",
-            nativeQuery = true)
-    List<Transaction> listarTransactions();
+
+    List<Transaction> findByTxId(String id);
 }
--- src/main/java/com/example/lab2_g5/Repository/UserRepository.java
+++ src/main/java/com/example/lab2_g5/Repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.example.lab2_g5.Repository;
 
+import com.example.lab2_g5.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
